7db5ad1f4943a4f433caef4fd278b6c3803c2cc2
Miguel Silva <miguelcfsilva@gmail.com>
2021-03-09 22:33:55 +0000

    Hawkins 1.4

--- src/mcts.py
+++ src/mcts.py
@@ -16,121 +16,43 @@ class tree:
         self.visits = 0
         self.score = 0
         self.children = []
-class mcts:
+class hawkins:
     def search(self, mx, player, last_move, castling_chance):
         global transposition_table
         global useful
-        depth = 3
-        root = tree(mx)
-        cicles = 0
-        starting_point = time.time()
-        while time.time() - starting_point <= 15:
-            cicles += 1
-            leaf = mcts.expand(self, root.board, player, root, last_move, castling_chance)
-            result = mcts.rollout(self, leaf, last_move, castling_chance, depth)
-            mcts.backpropagate(self, leaf, root, result)
-        print(len(transposition_table), cicles)
-        print(useful)
-        useful = 0
-        transposition_table = {}
-        return mcts.best_child(self, root).board
-
-    def expand(self, mx, player, root, last_move, castling_chance):
         global white_pieces
         global black_pieces
+        root = tree(mx)
         black_castling = [True if x != 0 else False for x in castling_chance][2:]
+
         plays = []
         matrices = generator.possible_matrix(mx, player, black_pieces, last_move, black_castling)[0] #all possible plays
-        if root.visits == 0:
-            for matrix in matrices:
-                child_node = tree(matrix)
-                plays.append(child_node)
-            for child in plays:
-                root.children.append(child) #create child_nodes in case they havent been created yet
-        for child in root.children:
-            if child.visits == 0:
-                return child #first iterations of the loop
-        return mcts.expansion_choice(self, root) #choose the one with most potential
 
-    def rollout(self, leaf, last_move, castling_chance, depth):
-        global transposition_table
-        global white_pieces
-        global black_pieces
-        global useful
-        score = 0
-        mx = leaf.board
-        swap = 1
-        level = 0 
-        black_castling = [True if x != 0 else False for x in castling_chance][2:]
-        white_castling = [True if x != 0 else False for x in castling_chance][:2]
-        abroll_score = mcts.minimax(self, mx, 3, -1*10**6, 1*10**6, False, castling_chance, last_move)
-        print(abroll_score)
-        score += abroll_score
-        return abroll_score
-        """
-        while mcts.material_left(self, mx) and level <= depth:
-            if swap == 1: # "White's" playing
-                possible_states = generator.possible_matrix(mx, "White", white_pieces, last_move, white_castling)[0]
-                if len(possible_states) == 0:
-                    if rules.is_attacked(mx, "White", white_pieces, last_move, False):
-                        transposition_table[mx] = 300
-                        return 300
-                    transposition_table[mx] = 0
-                    return 0
-                if len(possible_states) == 1:
-                    mx =  possible_states[0]
-                else:
-                    choice = random.randrange(0, len(possible_states))
-                    mx = possible_states[choice]
-                if True in white_castling:
-                    if mx[7*8+4] != "K":
-                        white_castling = [False, False]
-                    else:
-                        if white_castling[0] == True and mx[7*8] != "R":
-                            white_castling[0] = False
-                        if white_castling[1] == True and mx[7*8 + 7]!= "R":
-                            white_castling[1] = False
-
-            elif swap == 0: # "Black" playing
-                if True in black_castling:
-                    if mx[4] != "k":
-                        black_castling = [False, False]
-                    else:
-                        if black_castling[0] == True and mx[0] != "r":
-                            black_castling[0] = False
-                        if black_castling[1] == True and mx[7]!= "r":
-                            black_castling[1] = False
-
-                possible_states = generator.possible_matrix(mx, "Black", black_pieces, last_move, black_castling)[0]
-                if len(possible_states) == 0:
-                    print(level)
-                    if rules.is_attacked(mx, "Black", black_pieces, last_move, False):
-                        transposition_table[mx] = -300
-                        return -300
-                    transposition_table[mx] = 0
-                    return 0
-                if len(possible_states) == 1:
-                    mx =  possible_states[0]
-                else:
-                    choice = random.randrange(0, len(possible_states))
-                    mx = possible_states[choice]
-            level += 1
-            swap += 1
-            swap = swap % 2
-        if mx in transposition_table.keys():
-            return transposition_table[mx]
-        useful += 1
-        transposition_table[mx] = points.evaluate(mx)
-        #print(transposition_table[mx]) #this is a placeholder for a evaluation function
-        score += 0.3 * transposition_table[mx]
-        return score
-        """
-    def backpropagate(self, leaf, root, result): # updating our prospects stats
-        leaf.score += result
-        leaf.visits += 1
-        root.visits += 1
+        for matrix in matrices:
+            child_node = tree(matrix)
+            plays.append(child_node)
+        for child in plays:
+            root.children.append(child) #create child_nodes in case they havent been created yet
+        cicles = 1
+        starting_point = time.time()
+        for depth in range(1, 4):
+            for child in root.children:
+                child.score = hawkins.minimax(self, child.board, depth, -1*10**6, 1*10**6, False, castling_chance, last_move)
+            print(depth+1)
+            print(cicles)
+            root.children = sorted(root.children, key = lambda child: child.score, reverse = True)
+            best_move = hawkins.best_child(self, root).board
+            if time.time() - starting_point >= 10:
+                transposition_table = {}
+                return best_move
+            else:
+                transposition_table = {}
+                continue
+        print(cicles)
+        return best_move
 
     def minimax(self, mx, depth, alpha, beta, maximizing_player, castling_chance, last_move):
+        #print(depth)
         global transposition_table
         global useful
         global white_pieces
@@ -153,9 +75,7 @@ class mcts:
                         white_castling[0] = False
                     if white_castling[1] == True and mx[7*8 + 7]!= "R":
                         white_castling[1] = False
-            player = "White"
-            pieces = white_pieces
-            #print(mx)
+            player, pieces = "White", white_pieces
             possible_states = generator.possible_matrix(mx, "White", white_pieces, last_move, white_castling)[0]
         else:
             if True in black_castling:
@@ -166,8 +86,7 @@ class mcts:
                         black_castling[0] = False
                     if black_castling[1] == True and mx[7]!= "r":
                         black_castling[1] = False            
-            player = "Black"
-            pieces = black_pieces
+            player, pieces = "Black", black_pieces
             possible_states = generator.possible_matrix(mx, "Black", black_pieces, last_move, black_castling)[0]
         if len(possible_states) == 0:
             if rules.is_attacked(mx, player, pieces, last_move, False):
@@ -187,7 +106,7 @@ class mcts:
                 if state in transposition_table.keys():
                     eval = transposition_table[state]
                 else:
-                    eval = mcts.minimax(self, state, depth-1, alpha, beta, False, castling_chance, last_move)
+                    eval = hawkins.minimax(self, state, depth-1, alpha, beta, False, castling_chance, last_move)
                     transposition_table[state] = eval
                 if eval > max_eval:
                     max_eval = eval
@@ -201,7 +120,7 @@ class mcts:
                 if state in transposition_table.keys():
                     eval = transposition_table[state]
                 else:
-                    eval = mcts.minimax(self, state, depth-1, alpha, beta, True, castling_chance, last_move)
+                    eval = hawkins.minimax(self, state, depth-1, alpha, beta, True, castling_chance, last_move)
                     transposition_table[state] = eval
                 if eval < min_eval:
                     min_eval = eval
@@ -209,48 +128,16 @@ class mcts:
                 if beta <= alpha:
                     break
             return min_eval
-            
-
-
-    def material_left(self, mx):
-        king_counter = 0
-        minor_counter = 0
-        for i in mx:
-            if i.upper() in "K":
-                king_counter +=1
-            if i != "-" and i.upper() not in "K":
-                minor_counter +=1
-        if king_counter != 2:
-            return False
-        else:
-            return True
-
-
-    def calculate_score(self, score, child_visits, parent_visits, c): #UCB1
-        return score / child_visits + c * math.sqrt(math.log(parent_visits) / child_visits)
-
-    def expansion_choice(self, root): #returns most promising node
-        threshold = -1*10**6
-        for child in root.children:
-            potential = mcts.calculate_score(self, child.score, child.visits, root.visits, 1.414)
-            #print(potential)
-            #print(potential > threshold)
-            if potential > threshold:
-                #print("reached")
-                choice = child
-                threshold = potential
-        #print(mcts.calculate_score(self, choice.score, choice.visits, root.visits, 1.414), "here")
-        return choice
 
     def best_child(self,root):
         threshold = -1*10**6
         #print("we_got_here")
         for child in root.children:
-            print(points.evaluate(child.board))
+            #print(points.evaluate(child.board))
             if child.score > threshold:
                 win_choice = child
                 threshold = child.score
-        print("hey", points.evaluate(win_choice.board))
+        #print("hey", points.evaluate(win_choice.board))
         return win_choice
 
 
