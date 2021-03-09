bb57d4f678aa92a9e25da92d5664ae6e96266c44
Miguel Silva <miguelcfsilva@gmail.com>
2021-03-09 23:02:12 +0000

    Fixing Indentation

--- src/heuristic.py
+++ src/heuristic.py
@@ -1,12 +1,11 @@
 #this file will contain the eval function / heuristics needed to evaluate a each game state
 # reenter values for returned score, as of now, it can surpass winning score
 class points:
-    def evaluate(self, mx):
-        piece_value = {"P": -10, "Q": -90, "B": -30, "N": -30, "R": -50,
-                        "p": 10, "q": 90, "b": 30, "n": 30, "r": 50}
-        score = 0
+	def evaluate(self,mx):
+		piece_value = {"P": -10, "Q": -90, "B": -30, "N": -30, "R": -50, "p": 10, "q": 90, "b": 30, "n": 30, "r": 50}
+		score = 0
 		for pos in mx:
 			if pos.upper() in "-K":
 				continue
-			score += piece_value[mx]
+			score += piece_value[pos]
 		return score
